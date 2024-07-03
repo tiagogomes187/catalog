61d55e086dc0d1e0963a98d439c1a1e2afb805a6
Tiago Gomes <tiagogomes187@gmail.com>
2024-07-03 19:18:14 -0300

    Find Category by id, exception handling

---
+++ backend/src/main/java/br/dev/tiagogomes/catalog/resources/exceptions/StandardError.java
@@ -0,0 +1,24 @@
+package br.dev.tiagogomes.catalog.resources.exceptions;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.Instant;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class StandardError implements Serializable {
+	@Serial
+	private static final long serialVersionUID = 1L;
+
+	private Instant timestamp;
+	private Integer status;
+	private String error;
+	private String message;
+	private String path;
+
+}
--- backend/src/main/java/br/dev/tiagogomes/catalog/services/CategoryService.java
+++ backend/src/main/java/br/dev/tiagogomes/catalog/services/CategoryService.java
@@ -3,11 +3,13 @@ package br.dev.tiagogomes.catalog.services;
 import br.dev.tiagogomes.catalog.dto.CategoryDTO;
 import br.dev.tiagogomes.catalog.entities.Category;
 import br.dev.tiagogomes.catalog.repositories.CategoryRepository;
+import br.dev.tiagogomes.catalog.services.exceptions.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -21,5 +23,12 @@ public class CategoryService {
 		List<Category> list = repository.findAll();
 
 		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+	}
+
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+		return new CategoryDTO(entity);
 	}
 }
